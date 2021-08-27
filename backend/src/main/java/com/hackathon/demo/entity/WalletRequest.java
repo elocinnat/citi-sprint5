2b9ff6f00d4e55edb7d2d2661d686fa175e2567e
Administrator <wdn0612@gmail.com>
2021-08-27 06:45:22 +0000

    added user post mapping

---
+++ backend/src/main/java/com/hackathon/demo/entity/WalletRequest.java
@@ -0,0 +1,13 @@
+package com.hackathon.demo.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class WalletRequest {
+
+    private String type;
+    private double qty;
+
+}
